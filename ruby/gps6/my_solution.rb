69117a2490f986bfb91d168eb0810551e1ec644d
Simon Swartzman <simon.swartzman@gmail.com>
2017-03-11 11:02:59 -0600

    Almost finish refactoring method refactoring

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -29,17 +29,24 @@ class VirusPredictor
   # Categorizes the scale of death based on population density and computes it, and then makes a print statement to display to user.
   def predicted_deaths()
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
-      number_of_deaths = (@population * 0.05).floor
-    end
+
+    scale = (((@population_density * 100) / 500).floor / 10).floor) / 10
+    scale = 0.4 if scale > 0.4
+    scale = 0.05 if scale < 0.05
+    number_of_deaths = (@population * scale).floor
+
+
+    # if @population_density >= 200
+    #   number_of_deaths = (@population * 0.4).floor
+    # elsif @population_density >= 150
+    #   number_of_deaths = (@population * 0.3).floor
+    # elsif @population_density >= 100
+    #   number_of_deaths = (@population * 0.2).floor
+    # elsif @population_density >= 50
+    #   number_of_deaths = (@population * 0.1).floor
+    # else
+    #   number_of_deaths = (@population * 0.05).floor
+    # end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
